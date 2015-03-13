aa4aa8400eeb2c53710ea6e757f4d9c8389e4943
Paul Iannazzo <somethingitalian@gmail.com>
2015-03-13 17:34:30 -0400

    bumped version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'boxxxie:tweetnacl',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'git@github.com:boxxxie/meteor-tweetnacl.git',
   // URL to the Git repository containing the source code for this package.
